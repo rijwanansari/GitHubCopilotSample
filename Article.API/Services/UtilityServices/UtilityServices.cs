9fc2b138b3c2f31f7678a6096857a78bded52ad3
rijwanansari <rijwan.7475@gmail.com>
2025-04-06 08:58:29 +0545

    Move GetActiveUserEmails_Poor and GetGreetingMessage_Poor methods to UtilityServices

--- Article.API/Services/UtilityServices/UtilityServices.cs
+++ Article.API/Services/UtilityServices/UtilityServices.cs
@@ -1,4 +1,5 @@
 ﻿using System.Text;
+using Blog.API.Model;
 namespace Blog.API.Services.UtilityServices;
 
 public class UtilityServices
@@ -115,6 +116,59 @@ public class UtilityServices
 
         return max;
     }
+
+    public List<string> GetActiveUserEmails_Poor(List<User> users)
+    {
+        List<string> emails = new List<string>();
+        foreach (var user in users)
+        {
+            if (user.IsActive)
+            {
+                emails.Add(user.Email);
+            }
+        }
+        return emails;
+    }
+
+    public string GetGreetingMessage_Poor(string name)
+    {
+        string message = "Hello, " + name + ". Today is " + DateTime.Now.ToString("D") + ".";
+        return message;
+    }
+
+    // public void PrintUserNames_Poor(List<int> userIds, ApplicationDbContext dbContext)
+    // {
+    //     foreach (var userId in userIds)
+    //     {
+    //         var user = dbContext.Users.FirstOrDefault(u => u.Id == userId);
+    //         Console.WriteLine(user?.Name);
+    //     }
+    // }
+
+    // public void ProcessLogs(List<AuditLog> logs)
+    // {
+    //     foreach (var log in logs)
+    //     {
+    //         if (log.Type == "Login")
+    //         {
+    //             if (!string.IsNullOrEmpty(log.User))
+    //             {
+    //                 Console.WriteLine("User logged in: " + log.User);
+    //             }
+    //         }
+    //         else if (log.Type == "DataChange")
+    //         {
+    //             if (log.Changes != null)
+    //             {
+    //                 Console.WriteLine("Data changed: " + string.Join(",", log.Changes));
+    //             }
+    //         }
+    //         // other types...
+    //     }
+    // }
+
+    //generate password hash
+    
 
 
 }
